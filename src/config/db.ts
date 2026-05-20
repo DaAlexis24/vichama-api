08251eafe8b2947dc52e0f39cf674f5fd6e0eb8d
DaAlexis <danielalsole24@gmail.com>
2026-05-20 13:45:41 +0200

    Update config db to Prisma

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,13 @@
-import { Pool } from "pg";
-import { env } from "./env.ts";
-import debug from "debug";
+import { env } from './env.ts';
+import debug from 'debug';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client.ts';
 
-const log = debug(`${env.PROJECT_NAME}:config-DB`);
+const log = debug(`${env.PROJECT_NAME}:configDB`);
+log('Loaded database connection');
 
-export const connectDB = async () => {
-    const pool = new Pool({
+export const connectDB = async (): Promise<PrismaClient> => {
+    const adapter = new PrismaPg({
         user: env.PGUSER,
         password: env.PGPASSWORD,
         host: env.PGHOST,
@@ -13,14 +15,21 @@ export const connectDB = async () => {
         database: env.PGDATABASE,
     });
 
+    const prisma = new PrismaClient({
+        adapter,
+    });
+
     try {
-        const client = await pool.connect();
-        log("Database connection established successfully!");
-        log("Connected to:", pool.options.database);
-        client.release();
+        await prisma.$connect();
+        const [info] = (await prisma.$queryRaw`SELECT current_database()`) as {
+            current_database: string;
+        }[];
+        log('Database connection established successfully!');
+        log('Connected to:', info?.current_database);
+        prisma.$disconnect();
     } catch (error) {
-        log("Error connecting to the database", error);
+        log('Error connecting to the database', error);
         throw error;
     }
-    return pool;
+    return prisma;
 };
