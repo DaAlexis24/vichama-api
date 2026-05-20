47051bbe795213e9a9780ef5dd09dc9a17a0e0e2
DaAlexis <danielalsole24@gmail.com>
2026-05-21 01:16:18 +0200

    Update HTTP errors

--- src/errors/http-error.ts
+++ src/errors/http-error.ts
@@ -1,8 +1,8 @@
-import { env } from "../config/env.ts";
-import debug from "debug";
+import { env } from '../config/env.ts';
+import debug from 'debug';
 
 const log = debug(`${env.PROJECT_NAME}:http-error`);
-log("Loading HTTP error class...");
+log('Loading HTTP error class...');
 
 export class HttpError extends Error {
     status: number;
@@ -15,12 +15,40 @@ export class HttpError extends Error {
     ) {
         super(message, options);
         this.status = status;
-        this.statusMessage = statusMessage || "";
-        log(
-            "Creating HTTP error: %o",
-            this.status,
-            this.statusMessage,
-            this.message,
-        );
+        this.statusMessage = statusMessage || '';
+        log('Creating HTTP error: %o', this.status, this.statusMessage);
+    }
+}
+
+export class BadRequestError extends HttpError {
+    constructor(message = 'Bad Request', options?: ErrorOptions | undefined) {
+        super(400, 'Bad Request', message, options);
+    }
+}
+
+export class UnauthorizedError extends HttpError {
+    constructor(message: string, options?: ErrorOptions | undefined) {
+        super(401, 'Unauthorized', message, options);
+    }
+}
+
+export class ForbiddenError extends HttpError {
+    constructor(message: string, options?: ErrorOptions | undefined) {
+        super(403, 'Forbidden', message, options);
+    }
+}
+
+export class NotFoundError extends HttpError {
+    constructor(message: string, options?: ErrorOptions | undefined) {
+        super(404, 'Not Found', message, options);
+    }
+}
+
+export class InternalServerError extends HttpError {
+    constructor(
+        message = 'Internal Server Error',
+        options?: ErrorOptions | undefined,
+    ) {
+        super(500, 'Internal Server Error', message, options);
     }
 }
