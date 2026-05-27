3af41060cd098de45209d68ad5bc1185fc551caa
DaAlexis <danielalsole24@gmail.com>
2026-05-27 19:57:19 +0200

    Create Swagger UI config

---
+++ src/config/swagger.ts
@@ -0,0 +1,27 @@
+import swaggerJSDoc, { type Options } from 'swagger-jsdoc';
+
+import { env } from './env.ts';
+
+const swaggerOptions: Options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Vichama Api',
+            version: '1.0.0',
+            description: 'API REST for managing playlists and songs',
+            contact: {
+                name: 'Daniel Soledad',
+                email: 'danielalsole24@gmail.com',
+            },
+            server: [
+                {
+                    url: `http://${env.PGHOST}:${env.PORT}/api`,
+                    description: 'Development server',
+                },
+            ],
+        },
+    },
+    apis: ['./src/**/router/*.ts', './src/**/entities/*.ts'],
+};
+
+export const swaggerSpec = swaggerJSDoc(swaggerOptions);
