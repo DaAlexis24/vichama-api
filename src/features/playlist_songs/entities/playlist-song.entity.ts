62f0d2617f1dccd594501cf8b72b360c6be6db52
DaAlexis <danielalsole24@gmail.com>
2026-05-28 10:36:53 +0200

    Add PlaylistSong Docs powered by Swagger

--- src/features/playlist_songs/entities/playlist-song.entity.ts
+++ src/features/playlist_songs/entities/playlist-song.entity.ts
@@ -6,6 +6,27 @@ import { uuidSchema } from '../../entities/index.entity.ts';
 const log = debug(`${env.PROJECT_NAME}:entity:playlist-song`);
 log('Loaded playlistSong entities');
 
+/**
+ * @openapi
+ *
+ * components:
+ *   schemas:
+ *     Playlist:
+ *       type: object
+ *       required:
+ *         - playlist_id
+ *         - song_id
+ *       properties:
+ *         playlist_id:
+ *           type: string
+ *           format: uuid
+ *           example: 00111749-3efe-4458-8904-0e80bb617943
+ *         song_id:
+ *           type: string
+ *           format: uuid
+ *           example: 00111749-3efe-4458-8904-0e80bb617943
+ */
+
 export const PlaylistSongSchema = z.object({
     playlist_id: uuidSchema,
     song_id: uuidSchema,
--- src/features/playlist_songs/routes/playlist-song.route.ts
+++ src/features/playlist_songs/routes/playlist-song.route.ts
@@ -14,11 +14,78 @@ export class PlaylistSongRouter {
         this.#controller = controller;
         this.#router = Router();
 
+        /**
+         * @openapi
+         * tags:
+         *  - name: Playlist Song
+         *    description: API endpoints for managing songs into playlists
+         */
+
+        /**
+         * @openapi
+         * /playlists/{playlist_id}/{song_id}:
+         *   post:
+         *     summary: Añadir una canción a una playlist
+         *     tags:
+         *       - Playlist Song
+         *     parameters:
+         *       - in: path
+         *         name: playlist_id
+         *         required: true
+         *         schema:
+         *           type: string
+         *         description: ID de la playlist
+         *       - in: path
+         *         name: song_id
+         *         required: true
+         *         schema:
+         *           type: string
+         *         description: ID de la canción
+         *     responses:
+         *       201:
+         *         description: Canción agregada a la playlist
+         *         content:
+         *           application/json:
+         *             schema:
+         *               $ref: '#/components/schemas/PlaylistWithSongs'
+         *       404:
+         *         description: Playlist o canción no encontrada
+         *       500:
+         *         description: Error interno del servidor
+         */
         this.#router.post(
             '/:playlist_id/:song_id',
             this.#controller.addSongToPlaylist.bind(this.#controller),
         );
 
+        /**
+         * @openapi
+         * /playlists/{playlist_id}/{song_id}:
+         *   delete:
+         *     summary: Eliminar una canción de una playlist
+         *     tags:
+         *       - Playlist Song
+         *     parameters:
+         *       - in: path
+         *         name: playlist_id
+         *         required: true
+         *         schema:
+         *           type: string
+         *         description: ID de la playlist
+         *       - in: path
+         *         name: song_id
+         *         required: true
+         *         schema:
+         *           type: string
+         *         description: ID de la canción
+         *     responses:
+         *       204:
+         *         description: Canción eliminada de la playlist
+         *       404:
+         *         description: Playlist o canción no encontrada
+         *       500:
+         *         description: Error interno del servidor
+         */
         this.#router.delete(
             '/:playlist_id/:song_id',
             this.#controller.removeSongToPlaylist.bind(this.#controller),
