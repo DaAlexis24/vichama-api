9bb52fa3ac1a8f29499f3d6d32eb9246c3b4da08
DaAlexis <danielalsole24@gmail.com>
2026-05-30 18:40:56 +0200

    Add Home View powered by Layout class

--- src/views/core/layout.ts
+++ src/views/core/layout.ts
@@ -5,33 +5,53 @@ const log = debug(`${env.PROJECT_NAME}:app:view`);
 log('Loading app view class...');
 
 export class Layout {
-    static #title = env.PROJECT_NAME || 'Vichama API';
-    static #favicon = './favicon.ico';
+    #page: string;
+    #description: string;
+    #keywords: string;
+    #content: string;
+
+    static #favicon = '/favicon.png';
     static #css = './styles.css';
 
-    static render = () => {
+    constructor(
+        page: string,
+        description: string,
+        keywords: string,
+        content: string = '',
+    ) {
+        this.#page = page;
+        this.#description = description;
+        this.#keywords = keywords;
+        this.#content = content;
+    }
+
+    render = (): string => {
         const template = /*html*/ `
             <!doctype html>
-            <html lang="en">
+            <html lang="es">
             <head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Inicio | ${this.#title}</title>
-                <link rel="shortcut icon" href=${this.#favicon} type="image/x-icon">
-                <link rel="stylesheet" href=${this.#css}>
+                <meta name="description" content="${this.#description}" />
+                <meta name="keywords" content="${this.#keywords}" />
+                <title>${env.PROJECT_NAME} | ${this.#page}</title>
+                <link rel="shortcut icon" href="${Layout.#favicon}" type="image/x-icon">
+                <link rel="stylesheet" href="${Layout.#css}">
             </head>
             <body>
-                <header class="header">
-                    <h1>${this.#title}</h1>
-                </header>
-                <main>
-                    <section>
-                        Esto es una pruebas
-                    </section>
-                </main>
-                <footer class="footer">
-                    <p>Curso Desarrollo Web</p>
-                </footer>
+                <div class="grid-box">
+                    <header class="header">
+                        <h1>${this.#page}</h1>
+                    </header>
+                    <main>
+                        <section>
+                            ${this.#content}
+                        </section>
+                    </main>
+                    <footer class="footer">
+                        <p>Curso Desarrollo Web</p>
+                    </footer>
+                </div> 
             </body>
             </html>
             `;
